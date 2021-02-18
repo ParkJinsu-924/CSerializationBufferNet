eb2c7497dcb40ae5964b95a76773e480cdce4dbb
devPark <qkrwlstn924@naver.com>
2021-02-18 14:30:32 +0900

    Need to change stHEADER position

---
+++ CSerializationBufferNet.cpp
@@ -0,0 +1,235 @@
+#include "CSerializationBufferNet.h"
+
+int debugIndex = 0;
+
+CMemoryPoolTLS<CSerializationBuffer> CSerializationBuffer::memoryPool;
+
+CSerializationBuffer::CSerializationBuffer()
+{
+	chpBufferPtr = new char[eBUFFER_DEFAULT];
+	chpContentBufferPtr = chpWritePos = chpReadPos = chpBufferPtr + 5;
+	iBufferSize = eBUFFER_DEFAULT;
+	refCount = 0;
+}
+
+CSerializationBuffer::~CSerializationBuffer()
+{
+	delete chpBufferPtr;
+}
+
+void CSerializationBuffer::Release()
+{
+	this->~CSerializationBuffer();
+}
+
+void CSerializationBuffer::Clear()
+{
+	chpWritePos = chpReadPos = chpBufferPtr + 5;
+	bEncodeFlag = false;
+}
+
+int CSerializationBuffer::GetBufferSize()
+{
+	return iBufferSize;
+}
+
+bool CSerializationBuffer::Encode(CSerializationBuffer* pPacket)
+{
+	if (pPacket->bEncodeFlag)
+		return false;
+
+	stHEADER header;
+
+	header.code = 0x77;
+	header.length = pPacket->GetContentUseSize();
+	header.randKey = rand() % 256;
+	int sumCount = 0;
+	char* pContent = pPacket->GetContentBufPtr();
+	for (int i = 0; i < header.length; i++)
+	{
+		sumCount += *pContent;
+		pContent++;
+	}
+
+	header.checkSum = sumCount % 256;
+
+	pPacket->PutNetworkHeader((char*)&header, sizeof(header));
+
+	char key = 0x32;
+	char randomKey = header.randKey;
+	char* pPacketBuf = pPacket->GetContentBufPtr() - sizeof(UCHAR);
+	char thep = 0;
+	char resultBuf = 0;
+
+	for (int i = 1; i <= header.length + 1; i++)
+	{
+		thep = *pPacketBuf ^ (thep + randomKey + i);
+		resultBuf = *pPacketBuf = thep ^ (resultBuf + key + i);
+		pPacketBuf += 1;
+	}
+
+	pPacket->bEncodeFlag = true;
+	return true;
+}
+
+bool CSerializationBuffer::Decode(CSerializationBuffer* pPacket)
+{
+	stHEADER header;
+	pPacket->GetNetworkHeader((char*)&header, sizeof(header));
+
+	char key = 0x32;
+	char randomKey = header.randKey;
+	char* pPacketBuf = pPacket->GetContentBufPtr() - sizeof(UCHAR);
+	char thep = 0;
+	char ebuf, pbuf;
+
+	ebuf = *pPacketBuf;
+	pbuf = thep = *pPacketBuf ^ (key + 1);
+	*pPacketBuf = thep ^ (randomKey + 1);
+	pPacketBuf += 1;
+
+	for (int i = 2; i <= header.length + 1; i++)
+	{
+		thep = *pPacketBuf ^ (ebuf + key + i);
+		ebuf = *pPacketBuf;
+		*pPacketBuf = thep ^ (pbuf + randomKey + i);
+		pbuf = thep;
+		pPacketBuf += 1;
+	}
+
+	pPacket->GetNetworkHeader((char*)&header, sizeof(header));
+
+	UCHAR checkSum = 0;
+	int sumCount = 0;
+	char* pContent = pPacket->GetContentBufPtr();
+
+	for (int i = 0; i < header.length; i++)
+	{
+		sumCount += *pContent;
+		pContent++;
+	}
+	checkSum = sumCount % 256;
+	if (header.checkSum != checkSum)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+char* CSerializationBuffer::GetBufferPtr()
+{
+	return chpBufferPtr;
+}
+
+char* CSerializationBuffer::GetContentBufPtr()
+{
+	return chpContentBufferPtr;
+}
+
+int CSerializationBuffer::MoveWritePos(int iSize)
+{
+	chpWritePos += iSize;
+	return iSize;
+}
+
+int CSerializationBuffer::MoveReadPos(int iSize)
+{
+	chpReadPos += iSize;
+	return iSize;
+}
+
+int CSerializationBuffer::AddRef()
+{
+	InterlockedIncrement((long*)&refCount);
+	return refCount;
+}
+
+void CSerializationBuffer::DeqRef()
+{
+	if (InterlockedDecrement((long*)&refCount) == 0)
+	{
+		Free();
+	}
+}
+
+CSerializationBuffer& CSerializationBuffer::operator=(CSerializationBuffer& clSrcPacket)
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+	return *this;
+}
+
+//int CSerializationBuffer::GetData(char* chpDest, int iSize)
+//{
+//	memcpy(chpDest, chpReadPos, iSize);
+//	chpReadPos += iSize;
+//	return *chpDest;
+//}
+//
+//int CSerializationBuffer::PutData(char* chpSrc, int iSrcSize)
+//{
+//	memcpy(chpWritePos, chpSrc, iSrcSize);
+//	chpWritePos += iSrcSize;
+//	return *chpSrc;
+//}
+
+int CSerializationBuffer::PutContentData(char* chpSrc, int iSize)
+{
+	memcpy(chpWritePos, chpSrc, iSize);
+	chpWritePos += iSize;
+	return *chpSrc;
+}
+
+int CSerializationBuffer::GetContentData(char* chpSrc, int iSize)
+{
+	memcpy(chpSrc, chpReadPos, iSize);
+	chpReadPos += iSize;
+	return *chpSrc;
+}
+
+int CSerializationBuffer::PutNetworkHeader(char* chpSrc, int iSize)
+{
+	memcpy(chpBufferPtr, chpSrc, iSize);
+	return *chpSrc;
+}
+
+int CSerializationBuffer::GetNetworkHeader(char* chpSrc, int iSize)
+{
+	memcpy(chpSrc, chpBufferPtr, iSize);
+	return *chpSrc;
+}
+
+int CSerializationBuffer::GetContentUseSize()
+{
+	int retVal = chpWritePos - chpReadPos;
+	return retVal;
+}
+
+int CSerializationBuffer::GetTotalUseSize()
+{
+	return chpWritePos - chpBufferPtr;
+}
+
+int CSerializationBuffer::GetFreeSize()
+{
+	return chpBufferPtr + iBufferSize - chpWritePos;
+}
+
+CSerializationBuffer* CSerializationBuffer::Alloc()
+{
+	CSerializationBuffer* serialBufPtr = memoryPool.Alloc();
+
+	InterlockedIncrement((LONG*)&debugIndex);
+
+	serialBufPtr->Clear();
+
+	serialBufPtr->AddRef();
+
+	return serialBufPtr;
+}
+
+bool CSerializationBuffer::Free()
+{
+	InterlockedDecrement((LONG*)&debugIndex);
+	return memoryPool.Free(this);
+}
---
+++ CSerializationBufferNet.h
@@ -0,0 +1,96 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include "CMemoryPoolTLS\CMemoryPoolTLS.h"
+
+
+class CSerializationBuffer
+{
+	//메모리풀은 생성자를 호출해야하기 때문에
+	friend class CMemoryPoolTLS<CSerializationBuffer>;
+	friend class CNetServer;
+	enum enPACKET
+	{
+		eBUFFER_DEFAULT = 1400
+	};
+public:
+
+	//패킷파괴
+	void Release();
+
+	//패킷청소
+	void Clear();
+
+	//버퍼사이즈얻기
+	int GetBufferSize();
+
+	//버퍼포인터얻기
+	char* GetContentBufPtr();
+
+	//버퍼pos이동, 음수이동은 안됨
+	int MoveWritePos(int iSize);
+	int MoveReadPos(int iSize);
+
+	int AddRef();
+	void DeqRef();
+
+	CSerializationBuffer& operator=(CSerializationBuffer& clSrcPacket);
+
+	//int GetData(char* chpDest, int iSize);
+
+	//int PutData(char* chpSrc, int iSrcSize);
+
+	int PutContentData(char* chpSrc, int iSize);
+
+	int GetContentData(char* chpSrc, int iSize);
+
+	int PutNetworkHeader(char* chpSrc, int iSize);
+
+	int GetNetworkHeader(char* chpSrc, int iSize);
+
+	int GetContentUseSize();
+
+	int GetTotalUseSize();
+
+	int GetFreeSize();
+
+	static CSerializationBuffer* Alloc();
+
+	bool Free();
+
+private:
+#pragma pack(1)
+	struct stHEADER
+	{
+		UCHAR code;
+		USHORT length;
+		UCHAR randKey;
+		UCHAR checkSum;
+	};
+#pragma pack()
+private:
+	CSerializationBuffer();
+
+	virtual ~CSerializationBuffer();
+
+private:
+	int refCount;
+	int iBufferSize;
+	char* chpReadPos;
+	char* chpWritePos;
+	//버퍼의 시작점
+	char* chpBufferPtr;
+	//컨텐츠 버퍼의 시작점
+	char* chpContentBufferPtr;
+	
+	bool bEncodeFlag;
+	
+	int headerSize;
+
+	static CMemoryPoolTLS<CSerializationBuffer> memoryPool;
+
+private:
+	static bool Encode(CSerializationBuffer*);
+	static bool Decode(CSerializationBuffer*);
+	char* GetBufferPtr();
+};
